eb7232b9c0015143b9b95eefa618bcd6a589caca
William Martin <awmartin@gmail.com>
2011-09-26 09:40:15 -0400

    generate many thumbnails by giving generate:thumbnails a folder

--- lib/wren/commands/generate.rb
+++ lib/wren/commands/generate.rb
@@ -20,17 +20,28 @@ module Wren::Command
     # TODO: Put this in its own class and integrate with the rest of the app.
     # Generates small, medium, and large thumbnail images from a given path.
     def thumbnails
-      image_path = args.first
-      # 320x..., 640x..., 960x...
+      path = args.first
+      maxHeight = 500
+      web_image_types = [".png", ".jpg", ".gif"]
       
-      ext = File.extname(image_path)
-      image_path_small = image_path.gsub(ext, "-small#{ext}")
-      image_path_medium = image_path.gsub(ext, "-medium#{ext}")
-      image_path_large = image_path.gsub(ext, "-large#{ext}")
+      if File.directory?(path)
+        all_files = Dir[File.join(path, "**/*")]
+        images = all_files.select {|f| web_image_types.include?(File.extname(f))}
+      else
+        images = [path]
+      end
       
-      puts `cp #{image_path} #{image_path_small}; sips --resampleWidth 320 #{image_path_small}`
-      puts `cp #{image_path} #{image_path_medium}; sips --resampleWidth 640 #{image_path_medium}`
-      puts `cp #{image_path} #{image_path_large}; sips --resampleWidth 960 #{image_path_large}`
+      images.each do |image_path|
+        ext = File.extname(image_path)
+        
+        image_path_small = image_path.gsub(ext, "-small#{ext}")
+        image_path_medium = image_path.gsub(ext, "-medium#{ext}")
+        image_path_large = image_path.gsub(ext, "-large#{ext}")
+
+        puts `cp #{image_path} #{image_path_small}; sips --resampleWidth 320 #{image_path_small}`
+        puts `cp #{image_path} #{image_path_medium}; sips --resampleWidth 640 #{image_path_medium}`
+        puts `cp #{image_path} #{image_path_large}; sips --resampleWidth 960 #{image_path_large}`
+      end
     end
     
     def config
