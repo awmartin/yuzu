b0938585fc1cb4fd6471f511851519cf20bfd518
William Martin <awmartin@gmail.com>
2013-03-10 17:41:51 -0400

    fixed bugs in categories. added docstrings.

--- lib/yuzu/filters/categories.rb
+++ lib/yuzu/filters/categories.rb
@@ -7,6 +7,17 @@ import 'yuzu/filters/base'
 
 
 module Yuzu::Filters
+  # The Categories Filter enables the CATEGORIES() and CATEGORY() directives in posts, that enable
+  # an author to produce a set of content categories scoped to the blog or the entire site. These
+  # categories are managed and collected so Yuzu can generate HTML links for categories, generate
+  # filtered views that contain all posts of a given category, and so on.
+  #
+  # Categories can be specified for a given post with a comma-separated list:
+  #
+  #     CATEGORIES(design, process, user experience)
+  #
+  # These strings ("design", "process", "user experience") become Category objects in Yuzu's 
+  # internal representation.
   class CategoriesFilter < Filter
     def initialize
       @name = :categories
@@ -34,6 +45,11 @@ module Yuzu::Filters
   Filter.register(:categories => CategoriesFilter)
 
 
+  # Represents a single category given a String. An instance provides the mechanism to represent a
+  # link to the category's generated folder, a human-readable version of the category, etc.
+  #
+  # They are indexed by the lower-case, dashified version of the author-specified string. Thus, 
+  # "user experience" and "User Experience" become the same Category, but not "UserExperience".
   class Category
     attr_reader :name
 
@@ -42,20 +58,46 @@ module Yuzu::Filters
       @website_file = website_file
     end
 
+    # Returns the render-friendly name of the category, e.g. "User Experience".
+    def display_name
+      @name.titlecase
+    end
+
+    # Returns an HTML link to this category's generated folder. See the CategoryFoldersGenerator.
     def link
-      Html::Link.new(:href => url) << @name.titlecase
+      Html::Link.new(:href => url) << display_name
     end
 
+    # Return a Url object holding the link url to this Category's generated folder index. Again, see
+    # CategoryFoldersGenerator. If the category was specified in a blog post, the Category Url will
+    # refer to the blog subfolder for the category, e.g. blog/user-experience/index.html. If not, it
+    # will be a top-level folder. user-experience/index.html
     def url
-      blog_folder.link_url.to_s.gsub!("index.html", "") + @name + "/index.html"
+      if @website_file.in_blog?
+        blog_folder.currentpath + @name + "index.html"
+      else
+        siteroot.currentpath + @name + "index.html"
+      end
     end
 
+    # Returns a Path object that represents the folder's location on disk, as a file Path, not a
+    # Url (as the url method returns). Like url, this returns the path relative to the blog folder
+    # if the Category was specified in a blog post, otherwise, it returns a folder path relative to
+    # the root.
     def path
-      Helpers::Path.new(blog_folder.path.name, @name)
+      if @website_file.in_blog?
+        Helpers::Path.new(blog_folder.path.name, @name)
+      else
+        Helpers::Path.new(siteroot.path.name, @name)
+      end
     end
 
     def blog_folder
       @website_file.blog_folder
+    end
+
+    def siteroot
+      @website_file.root
     end
 
     def <=>(other)
