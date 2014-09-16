0049e978a2a6958d91519e51a6bbc01cb1f293de
William Martin <awmartin@gmail.com>
2014-09-16 14:07:29 -0400

    Fix to description filter

--- lib/yuzu/filters/description.rb
+++ lib/yuzu/filters/description.rb
@@ -12,6 +12,11 @@ module Yuzu::Filters
     def default(website_file=nil)
       ""
     end
+
+    def get_value(website_file)
+      val = match(website_file.raw_contents)
+      val.nil? ? nil : val.gsub("\n", "")
+    end
   end
   Filter.register(:description => DescriptionFilter)
 end
